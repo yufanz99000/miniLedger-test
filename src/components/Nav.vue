4ed1061266b28fcd6bfd11837e6eed6394de96b8
vivi <15961591506@163.com>
2020-05-12 19:41:14 +0800

    create <Icon/>

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,25 @@
 <template>
     <div class="nav">
-        <router-link to="/money">记账</router-link>|
-        <router-link to="/labels">标签</router-link>|
-        <router-link to="statistics">统计</router-link>
+        <router-link to="/money">
+            <Icon name="ledger"/>
+            账本
+        </router-link>|
+
+        <router-link to="/labels">
+            <Icon name="money"/>
+            记账
+            </router-link>|
+
+        <router-link to="statistics">
+            <Icon name="histogram"/>
+            统计
+        </router-link>
     </div>
 </template>
 
 <script lang="ts">
+    //import x from '@/assets/icons/money.svg';
+    //console.log(x);
     export default {
         name: 'Nav'
     };
