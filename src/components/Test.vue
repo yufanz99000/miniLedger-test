bd4741680208af52b63e5eb9ed2d351b1d9f4b24
vivi <15961591506@163.com>
2020-05-12 11:08:58 +0800

    create 3 routes

---
+++ src/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Test'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Labels.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Labels.vue</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Lables'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
