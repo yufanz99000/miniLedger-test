df9f8b9d6b7a4dad02c6c194a981751d4d93e0f2
vivi <15961591506@163.com>
2020-05-13 19:00:07 +0800

    finishNumberPad

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,34 +1,71 @@
 <template>
     <div class="numberPad">
-        <div class="output">100</div>
+        <div class="output">{{output}}</div>
         <div class="buttons">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>删除</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>清空</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button class="OK">OK</button>
-            <button class="zero">0</button>
-            <button>.</button>
+            <button @click="inputContent">1</button>
+            <button @click="inputContent">2</button>
+            <button @click="inputContent">3</button>
+            <button @click="remove">删除</button>
+            <button @click="inputContent">4</button>
+            <button @click="inputContent">5</button>
+            <button @click="inputContent">6</button>
+            <button @click="clear">清空</button>
+            <button @click="inputContent">7</button>
+            <button @click="inputContent">8</button>
+            <button @click="inputContent">9</button>
+            <button @click="ok" class="OK">OK</button>
+            <button @click="inputContent" class="zero">0</button>
+            <button @click="inputContent">.</button>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'NumberPad'
-    };
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+
+    @Component
+    export default class NumberPad extends Vue {
+        output = '';
+
+        inputContent(event: MouseEvent) { //添加鼠标事件
+            const button = (event.target as HTMLButtonElement);
+            const input = button.textContent as string; //不为空
+            if (this.output.length === 16) { return; }
+            if (this.output === '0') {
+                if ('0123456789'.indexOf(input) >= 0) {
+                    this.output = input;
+                } else { //如果输入.
+                    this.output += input;
+                }
+                return;
+            }
+            if (this.output.indexOf('.') >= 0 && input === '.') {
+                return;
+            }
+            this.output += input;
+        }
+
+        remove() {
+            if (this.output.length === 1) {
+                this.output = '0'
+            } else {
+                this.output = this.output.slice(0, -1)
+            }
+        }
+        clear(){
+            this.output = '0'
+        }
+        ok(){
+
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
     @import "~@/assets/styles/helper.scss";
     @import "~@/assets/styles/reset.scss";
+
     .numberPad {
         .output {
             @extend %clearFix;
@@ -37,6 +74,7 @@
             font-family: Consolas, monospace;
             padding: 9px 16px;
             text-align: right;
+            height: 72px;
         }
 
         .buttons {
--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,16 +1,34 @@
 <template>
     <div>
         <ul class="types">
-            <li class="selected">支出</li>
-            <li>收入</li>
+            <li :class="type === '-' && 'selected'"
+                @click="selectType('-')">支出</li>
+            <li :class="type === '+' && 'selected'"
+                @click="selectType('+')">收入</li>
         </ul>
     </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'Types'
-    };
+    import Vue from 'vue'
+    import {Component, Prop} from 'vue-property-decorator';
+
+    @Component //使用装饰器
+    //告诉ts以下代码是vue的组件，type会被处理成data，selectType会被处理成methods
+    export default class Types extends Vue{
+        type = '-' //-表示支出， +表示收入
+
+        @Prop(Number) xxx: number | undefined
+        //prop告诉vue xxx不是data是prop
+        //number告诉vue xxx是个number
+
+        selectType(type: string) {
+            if (type !== '-' && type !== '+'){
+                throw new Error('type is undefined')
+            }
+            this.type = type
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
